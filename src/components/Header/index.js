84d1bd6c946175ece82d6e47a371860076372b64
Tyler J. Perry <iamtylerjordan@gmail.com>
2024-03-09 22:29:39 -0500

    wip

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './styles/header.scss';
+
+const Header = () => {
+    return (
+        <>
+            <header className="header">
+                <div className="container">
+                    <div className="test">hi</div>
+                    <div className="test">bye</div>
+                </div>
+            </header>
+        </>
+    );
+}
+
+export default Header;
