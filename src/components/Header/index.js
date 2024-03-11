61563c6902b1791863d77145fd9b5d933ef0bce4
Tyler J. Perry <iamtylerjordan@gmail.com>
2024-03-11 02:10:58 -0400

    wip: laying out header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import './styles/header.scss';
 
 const Header = () => {
+    const logo = `${process.env.PUBLIC_URL}/logo.svg`;
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <>
-            <header className="header">
-                <div className="header__nav container">
+        <header className="header">
+            <div className="mobile-nav container">
+                <div className="mobile-nav__bar">
                     <div className="logo">
-                        <img src={`${process.env.PUBLIC_URL}/logo.svg`} />
+                        <img src={logo} />
                     </div>
-                    <div className="menu-btn"></div>
+                    <div 
+                    className="menu-btn" 
+                    onClick={(e) => {
+                        // TODO: toggle menu button class
+                    }}></div>
                 </div>
-            </header>
-        </>
+                <div className={`mobile-nav__menu ${menuOpen ? 'is-open' : ''}`}></div>
+            </div>
+            <div className="desktop-nav container">
+                <div className="desktop-nav">
+                    <div className="logo">
+                        <img src={logo} />
+                    </div> 
+                    <div className="nav-menu">
+                        {/* TODO: add page anchors */}
+                        <ul>
+                            <li><a href={''}>Recent work</a></li>
+                            <li><a href={''}>Experience</a></li>
+                            <li><a href={''}>About me</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
