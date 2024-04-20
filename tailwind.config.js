b8fe36247142289f9b0a1afd5d2379cff019530e
Tyler J. Perry <iamtylerjordan@gmail.com>
2024-04-20 16:33:39 -0400

    installing support for tw nesting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,scss}"],
+  content: ["./src/**/*.{html,js,jsx,css,scss}"],
   theme: {
     extend: {},
   },
