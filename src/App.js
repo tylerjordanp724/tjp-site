a0eebab8c741c0296a9912aa34858492d0e173b8
Tyler J. Perry <iamtylerjordan@gmail.com>
2025-03-10 22:43:49 -0400

    fix(tjp-site): removed unused motion import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MomentumScroll from './components/MomentumScroll';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Footer from './components/Footer';
-import { motion } from 'framer-motion';
 
 function App() {
   return (
